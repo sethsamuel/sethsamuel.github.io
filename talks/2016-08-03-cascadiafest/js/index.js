0dea20419a6a13e3403b76710222b4beb96773ec
Seth Samuel <sethfsamuel@gmail.com>
2016-07-16 11:23:09 -0400

    Talk framework

---
+++ talks/2016-08-03-cascadiafest/js/index.js
@@ -0,0 +1,6 @@
+var Reveal = require('reveal.js');
+var RevealCSS = require('reveal.js/css/reveal.css');
+var RevealCSS = require('reveal.js/css/theme/moon.css');
+
+Reveal.initialize();
+console.log('❤');
---
+++ talks/2016-08-03-cascadiafest/webpack.config.js
@@ -0,0 +1,83 @@
+"use strict";
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var _ = require('lodash');
+
+const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || "8888";
+
+var pages = ['index', 'matrix', 'gradient'];
+
+function entryPoint(entry) {
+	return [
+		`webpack-dev-server/client?http://${HOST}:${PORT}`, // WebpackDevServer host and port
+		`webpack/hot/only-dev-server`,
+		`./js/${entry}.js`,
+	];
+}
+
+module.exports = {
+	entry: _.zipObject(pages, _.map(pages, entryPoint)),
+
+	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: '[name].js'
+	},
+	resolve: {
+		extensions: ['', '.js']
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loaders: ['babel']
+			},
+			{
+				test: /\.pug$/,
+				exclude: /node_modules/,
+				loader: 'pug-html-loader'
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "file"
+			},
+			{
+				test: /\.(woff|woff2)$/,
+				loader: "url?prefix=font/&limit=5000"
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=application/octet-stream"
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=image/svg+xml"
+			},	
+		]
+	},
+	devServer: {
+		contentBase: "./public",
+		noInfo: true, //  --no-info option
+		hot: true,
+		inline: true,
+		port: PORT,
+		host: HOST
+		},
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+	].concat(_.map(pages, (page) => {
+		return new HtmlWebpackPlugin({
+			chunks: [`${page}`],
+			template: `${page}.pug`,
+			filename: `${page}.html`
+		});
+	}))
+};
