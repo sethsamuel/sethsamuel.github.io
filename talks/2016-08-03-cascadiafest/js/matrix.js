45ca91da99f73ab01387aff0e17fc62ea25a2fee
Seth Samuel <sethfsamuel@gmail.com>
2016-07-25 00:28:59 -0400

    Matrix demo

--- talks/2016-08-03-cascadiafest/js/matrix.js
+++ talks/2016-08-03-cascadiafest/js/matrix.js
@@ -4,7 +4,7 @@ import '../css/matrix.css';
 console.log('import?');
 console.log('MATRIX DEMO');
 
-let size = 256;
+let size = 5;
 let inputLeft = [];
 let inputRight = [];
 let resultCpu = [];
@@ -24,6 +24,7 @@ const generateMatrices = () => {
 	for(let i = 0; i < size * size; i++) {
 		inputLeft.push(Math.round(Math.random()*10));
 		inputRight.push(Math.round(Math.random()*10));
+		// inputRight.push(1);
 		resultCpu.push('&nbsp;');
 		resultGpu.push('&nbsp;');
 	}
@@ -36,7 +37,7 @@ const generateMatrices = () => {
 	document.querySelector('.gpu-time').innerHTML = '&nbsp';
 }
 
-size = document.querySelector('.size-value').value = document.querySelector('.size').value;
+document.querySelector('.size-value').value = document.querySelector('.size').value = size;
 
 document.querySelector('.size').addEventListener('input', (e) => {
 	size = document.querySelector('.size-value').value = document.querySelector('.size').value;
@@ -57,8 +58,8 @@ document.body.addEventListener('keypress', (e) => {
 });
 
 const offsetForPoint = (point) => {
-  // return 4 * (point[1] * size + point[0]);
-  return point[1] * size + point[0];
+	// return 4 * (point[1] * size + point[0]);
+	return point[1] * size + point[0];
 }
 
 const dataAtPoint = (data, point) => {
@@ -73,6 +74,33 @@ const toWolframString = (data) => {
 		rows.push('{' + cols.join(',') + '}');
 	}
 	return '{' + rows.join(',') + '}';
+}
+
+const toImageDataArray = (data) => {
+	console.log(data);
+	const array = [];
+	for(let i = 0; i < data.length; i++) {
+		array.push(0);
+		array.push(0);
+		array.push(0);
+		array.push(data[i]);
+	}
+	console.log(array);
+	return new Uint8ClampedArray(array);
+};
+
+const fromImageDataArray = (data) => {
+	console.log(data);
+	const array = [];
+	for(let i = 0; i< data.length; i += 4){
+		const value = data[i + 3] +
+			(data[i + 2] << 8 >>> 0) +
+			(data[i + 1] << 16 >>> 0) +
+			(data[i] << 24 >>> 0);
+		array.push(value);
+	}
+	console.log(array);
+	return array;
 }
 
 const goCpu = document.querySelector('.go-cpu');
@@ -103,43 +131,73 @@ goCpu.addEventListener('click', (e) => {
 	}, 100);
 });
 
-(() => {
-	const canvas = document.querySelector("canvas");
+
+const goGpu = document.querySelector('.go-gpu');
+goGpu.addEventListener('click', (e) => {
+	document.querySelector('.spinner').classList.add('active');
+	e.target.setAttribute('disabled', true);
+	setTimeout(() => {
+		const startTime = performance.now();
+		calculateGpuResult((err, result) => {
+			if(err) {
+				return console.error(err);
+			}
+			const elapsed = performance.now() - startTime;
+			document.querySelector('.gpu-time').innerText = elapsed.toFixed(2) + 'ms';
+			gpuResult.matrix = resultGpu = result;
+			document.querySelector('.spinner').classList.remove('active');
+			e.target.removeAttribute('disabled');
+		});
+	}, 100);
+});
+
+const calculateGpuResult = (done) => {
+	// const canvas = document.querySelector("canvas");
+	const canvas = document.createElement('canvas');
+
+	canvas.width = canvas.height = size;
 	const gl = canvas.getContext('webgl');
-	let uniformColor;
 	let vertexBuffer;
 	let vPosition;
-	let texture;
+	let textureLeft;
+	let textureRight;
+	let uSamplerLeft;
+	let uSamplerRight;
 
-	gl.clearColor(0,0,0,1);
+	gl.clearColor(0,0,0,0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.enable(gl.DEPTH_TEST);
 	gl.depthFunc(gl.LEQUAL);
 	// gl.viewport(0, 0, canvas.width, canvas.height);
 
-	const thresholdInput = document.querySelector('input');
-
 	initShaders();
 	initBuffers();
-
+	initTextures();
 	draw();
 
 	function draw(){
-		return;
-		const timestamp = ((1+Math.sin((new Date()).getTime()/0xff0000))/2.0 * 0xffffff);
-		const r = ((timestamp & 0xff0000) >> 16)/255.0;
-		const g = ((timestamp & 0x00ff00) >> 8)/255.0;
-		const b = ((timestamp & 0x0000ff) >> 0)/255.0;
-
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 		gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-		gl.uniform3f(uniformColor, r, g, b);
 		gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, textureLeft);
+		gl.uniform1i(uSamplerLeft, 0);
+
+		gl.activeTexture(gl.TEXTURE1);
+		gl.bindTexture(gl.TEXTURE_2D, textureRight);
+		gl.uniform1i(uSamplerRight, 1);
 
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-		requestAnimationFrame(draw);
+		gl.flush();
+		let pixels = new Uint8Array(size * size * 4);
+		gl.readPixels(0, 0, size, size, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+		console.log('pixels', pixels);
+		console.log(pixels.length);
+		const outputData = fromImageDataArray(pixels);
+		return done(null, outputData);
 	}
 
 	function initBuffers(){
@@ -172,21 +230,82 @@ goCpu.addEventListener('click', (e) => {
 
 		gl.useProgram(shaderProgram);
 
-		uniformColor = gl.getUniformLocation(shaderProgram, "aColor");
+		uSamplerLeft = gl.getUniformLocation(shaderProgram, "uSamplerLeft");
+		uSamplerRight = gl.getUniformLocation(shaderProgram, "uSamplerRight");
+
 		vPosition = gl.getAttribLocation(shaderProgram, "vPosition");
 		gl.enableVertexAttribArray(vPosition);
 	}
+
+	function initTextures() {
+		{
+			textureLeft = gl.createTexture();
+			const imageData = new ImageData(toImageDataArray(inputLeft), size, size);
+			// tmpContext.putImageData(imageData, 0, 0);
+			console.log('left imageData',imageData.data);
+
+
+			gl.bindTexture(gl.TEXTURE_2D, textureLeft);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			gl.bindTexture(gl.TEXTURE_2D, null);
+		}
+
+		{
+			textureRight = gl.createTexture();
+
+			const imageData = new ImageData(toImageDataArray(inputRight), size, size);
+			console.log('right imageData',imageData.data);
+
+			// tmpContext.putImageData(imageData, 0, 0);
+			gl.bindTexture(gl.TEXTURE_2D, textureRight);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			gl.bindTexture(gl.TEXTURE_2D, null);
+		}
+	}
+
 
 	function getFragmentShader(){
 		const shader = gl.createShader(gl.FRAGMENT_SHADER);
 		const source = `
 			precision highp float;
-			uniform vec3 aColor;
+			uniform sampler2D uSamplerLeft;
+			uniform sampler2D uSamplerRight;
 			varying vec2 vCoord;
+
+			float floatFromVec4(vec4 v) {
+				return (v.r*pow(2.0,24.0) + v.g*pow(2.0,16.0) + v.b*pow(2.0,8.0) + v.a*pow(2.0,0.0));
+			}
+
+			// Row = t, col = s
 			void main(void) {
-				gl_FragColor = vec4(vCoord.s, vCoord.t, 0, 1.0);
+				float sum = 0.0;
+				for(int i = 0; i < ${size}; i++) {
+					vec4 cLeft = texture2D(uSamplerLeft, vec2(float(i)/${size}.0,vCoord.t));
+					vec4 cRight = texture2D(uSamplerRight, vec2(vCoord.s,float(i)/${size}.0));
+					sum = sum + (floatFromVec4(cLeft)*255.0 * floatFromVec4(cRight)*255.0);
+				}
+
+				float a = (mod(sum, pow(2.0,8.0)));
+				float b = (mod(sum - a, pow(2.0,16.0))) * pow(2.0, -8.0);
+				float g = (mod(sum - b - a, pow(2.0,24.0))) * pow(2.0, -16.0);
+				float r = (mod(sum - g - b - a, pow(2.0,32.0))) * pow(2.0, -24.0);
+				gl_FragColor = vec4(
+					r/255.0,
+					g/255.0,
+					b/255.0,
+					a/255.0
+				);
 			}
 		`;
+		console.log(source);
 		gl.shaderSource(shader, source);
 		gl.compileShader(shader);
 		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
@@ -205,7 +324,7 @@ goCpu.addEventListener('click', (e) => {
 			attribute vec3 vPosition;
 			varying vec2 vCoord;
 			void main(void) {
-				vCoord = vec2((vPosition.s+1.0)/2.0, 1.0-(vPosition.t+1.0)/2.0);
+				vCoord = vec2((vPosition.s+1.0)/2.0, (vPosition.t+1.0)/2.0);
 				gl_Position = vec4(vPosition, 1.0);
 			}
 		`;
@@ -220,4 +339,4 @@ goCpu.addEventListener('click', (e) => {
 
 		return shader;
 	}
-})()
+};
